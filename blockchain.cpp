9826d1a4c36e24dfdb302665f53f3d53c9a41e7f
sumagnadas <sumagnadas@gmail.com>
2025-06-19 02:32:49 +0530

    Add a proof of work.

--- blockchain.cpp
+++ blockchain.cpp
@@ -3,7 +3,9 @@
 #include <ctime>
 #include <bits/stdc++.h>
 using namespace std;
-string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ. ";
+
+// custom base64 encoding of all alphabets plus . and space as i required them for atleast some better looking data
+string base = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ. ";
 /*
     Overall structure:
     1) block structure (struct or class)
@@ -36,25 +38,33 @@ public:
     string prevHash;
     string hash;
     block_structure *next;
+    int difficulty;
     unsigned long long int nonce;
 
     block_structure(string in_transac)
     {
         timestamp = std::time(nullptr);
         transaction = in_transac;
+        difficulty = ((in_transac.length() + blockNo) % 32) / 4 + 6;
         next = nullptr;
     }
 };
 
 // This is the hashing function. I had to declare this before anything else cuz it was giving error. Hence (5) complete
-string hash_function(block_structure *block) //,string prevHash,string hash,unsigned long long int nonce)
+string hash_function(block_structure *block)
 {
+    // This hashing function is a very simple function inspired from SHA256 algorithm which i read up before making this
+    // This function has 4 parts ->
+    // Initial hashes
     vector<unsigned int> hashes(4);
     hashes[0] = 0x12df9d06;
     hashes[1] = 0x71bd5b64;
     hashes[2] = 0x4881feef;
     hashes[3] = 0xca6a7e80;
-    string new_transac = to_string(block->blockNo) + to_string(block->timestamp) + block->prevHash + block->transaction + to_string(block->nonce);
+
+    string new_transac = to_string(block->blockNo) + to_string(block->timestamp) + to_string(block->nonce) + block->prevHash + block->transaction; // Include all the data of the block to create hashes
+
+    // Stretching input to be able to break it into fixed sized blocks of 256 characters
     int len = new_transac.size() % 256;
     while (len and len < 128 and new_transac.size() < 256)
     {
@@ -62,41 +72,77 @@ string hash_function(block_structure *block) //,string prevHash,string hash,unsi
         len = new_transac.size() % 256;
     }
     new_transac += new_transac.substr(0, 256 - len);
+    // Divide into blocks and store it into arrays
     unsigned long long int blocks = new_transac.size() / 256;
     vector<string> arr(blocks);
     for (unsigned long long int i = 0; i < blocks; i++)
-    {
         arr[i] = new_transac.substr(i * 256, 256);
-    }
+
+    // for each block, there will be 4 4-byte hashes
     auto data_hashes = new unsigned int[blocks][4];
+    // convert from string to integer using a custom base64 encoding which is defined by the base variable
     for (auto i = 0; i < blocks; i++)
     {
         auto x = arr[i];
         vector<string> data_arr(4);
         for (int j = 0; j < 4; j++)
         {
+            // each block contains 4*32 characters
+            // these 32-char chunks will each be converted into a hash for the block
             string data = x.substr(j * 32, 32);
-            unsigned int data_hash = 0;
-            for (int k = 0; k < 4; k++)
+            unsigned int data_hash = 0; // will contain one hash of the block during iteration
+
+            for (int k = 0; k < 4; k++, data_hash <<= 8) // shift the hash 8 bits for new byte
             {
+                // each 32-char chunks can be divided into 4 parts
+                // each 8-char part will be one byte of the 4-byte hash
                 string str = data.substr(k * 8, 8);
 
                 unsigned int data_byte = 0;
-                for (auto x : str)
-                {
-                    data_byte |= findParity(chars.find(x));
-                    data_byte <<= 1;
-                }
+                // due to the base selected, each char of the string gives out a 6bit value i.e. a value within 0-63
+                // hence we take the value of each char and find if there's parity in it and include that as a bit
+                // for each 8 characters, eventually making a byte for that 8 character part.
+
+                for (auto x = str.begin(); x < str.end(); x++, data_byte <<= 1) // shift 1 place
+                    data_byte |= findParity(base.find(*x));                     // add that parity bit.
+
                 data_hash |= data_byte;
-                data_hash <<= 8;
+                // add the new byte to the hash
             }
             data_hashes[i][j] = data_hash;
         }
     }
+    // combine all the hashes of the blocks
     for (int i = 0; i < blocks; i++)
     {
         for (int j = 0; j < 4; j++)
-            hashes[j] ^= data_hashes[i][j];
+        {
+            // didnt use auto as i couldn't couldn't reassign the variable even when initialized with a dummy function with similar return type and parameters
+            unsigned int (*op)(unsigned int, unsigned int);
+            // for a bit more randomness of operation
+            switch ((i + j) % 3)
+            {
+            case 0:
+            {
+                op = [](unsigned int a, unsigned int b) -> unsigned int
+                { return a & b; };
+                break;
+            }
+            case 1:
+            {
+                op = [](unsigned int a, unsigned int b) -> unsigned int
+                { return a | b; };
+                break;
+            }
+            case 2:
+            {
+                op = [](unsigned int a, unsigned int b) -> unsigned int
+                { return a ^ b; };
+                break;
+            }
+            }
+            hashes[j] = op(hashes[j], data_hashes[i][j]);
+        }
     }
     char new_hash[35];
     sprintf(new_hash, "0x%08x%08x%08x%08x", hashes[0], hashes[1], hashes[2], hashes[3]);
@@ -112,6 +158,7 @@ block_structure *add_block_to_chain(block_structure *block, block_structure *hea
     // The only thing that is left to do is add it hence there would be no apparent "create_block" function
     if (head == nullptr)
     {
+        // just add it, no need to check
         head = block;
         block->hash = hash_function(block);
     }
@@ -122,9 +169,19 @@ block_structure *add_block_to_chain(block_structure *block, block_structure *hea
             temp = temp->next;
         block->prevHash = temp->hash;
         block->blockNo = temp->blockNo + 1;
-        unsigned long long int nonce = rand();
-        block->hash = hash_function(block);
-        block->nonce = nonce;
+        for (unsigned long long int i = 0;; i++)
+        {
+            block->nonce = i;
+            block->hash = hash_function(block);
+            auto start = block->hash.begin();
+            auto finish = block->hash.end();
+            // Proof of work
+            // basically the nonce has to be found such that (no. of 0) > block difficulty (taken from bitcoin's PoW definition)
+            // the proof of work i have chosen is either letting it validate or it isn't for some reason, theres no inbetween
+            // like its going to take a lot of time or
+            if (count(start, finish, '0') <= block->difficulty)
+                break;
+        }
         temp->next = block;
     }
     return head;
@@ -154,7 +211,6 @@ void display_blockchain(block_structure *head)
 void del_blockchain(block_structure *head)
 {
     block_structure *temp = head;
-
     while (temp != NULL)
     {
         auto next = temp->next;
@@ -170,11 +226,27 @@ int main()
     genesis->prevHash = string("0x12df9d0671bd5b644881feefca6a7e80");
     genesis->nonce = 1234;
     genesis->blockNo = 0;
-    // cout << hash_function("1abracadabragiliglichuabracadabragiliglichuabracadabragiliglichuabracadabragiliglichuabracadabragiliglichuabracadabragiliglichuabracadabragiliglichuabracadabragiliglichuabracadabragiliglichuabracadabragiliglichuabracadabragiliglichuabracadabragiliglichuabracadabragiliglichuabracadabragiliglichuabracadabragiliglichu") << endl;
+
     head = add_block_to_chain(genesis, head);
     block_structure *new_block = new block_structure("2nd block");
     head = add_block_to_chain(new_block, head);
     display_blockchain(head);
+    char opt = 'y';
+    while (opt != 'n')
+    {
+        cout << "Want to add a node (y/n)? ";
+        cin >> opt;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        if (opt == 'y')
+        {
+            string dat;
+            cout << "Give data to add to blockchain: ";
+            getline(cin, dat);
+            block_structure *new_block = new block_structure(dat);
+            head = add_block_to_chain(new_block, head);
+            display_blockchain(head);
+        }
+    }
     // over here goes how the chain is to interact with the user
     // write some code over here to do the following:
     //  [in short, just call the functions that lead to these tasks]
